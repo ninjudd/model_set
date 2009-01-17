2ab7815dd6400d76c5f5f6788686033ccadafacc
Justin Balthrop <justin@geni.com>
2009-01-17 09:41:59 -0800

    use the ruby client included with sphinx instead of ultrasphinx for searching. there is a bug in riddle, and 64bit attributes don't work

--- lib/model_set/sphinx_query.rb
+++ lib/model_set/sphinx_query.rb
@@ -21,15 +21,19 @@ class ModelSet
       clear_cache!
     end
 
-    def order_by!(field, mode = :ascending)
-      raise "invalid mode: :#{mode}" unless [:ascending, :descending].include?(mode)
-      @sort_order = [mode, field]
-      clear_cache!
-    end
+    SORT_MODES = {
+      :relevance  => Sphinx::Client::SPH_SORT_RELEVANCE,
+      :descending => Sphinx::Client::SPH_SORT_ATTR_DESC,
+      :ascending  => Sphinx::Client::SPH_SORT_ATTR_ASC,
+      :time       => Sphinx::Client::SPH_SORT_TIME_SEGMENTS,
+      :extending  => Sphinx::Client::SPH_SORT_EXTENDED,
+      :expression => Sphinx::Client::SPH_SORT_EXPR,
+    }
 
-    def limit!(limit)
-      @limit  = limit  ? limit.to_i  : nil
-      clear_limited_cache!
+    def order_by!(field, mode = :ascending)
+      raise "invalid mode: :#{mode}" unless SORT_MODES[mode]
+      @sort_order = [SORT_MODES[mode], field]
+      clear_cache!
     end
 
     def size
@@ -50,45 +54,78 @@ class ModelSet
   private
 
     def fetch_results
-      opts = {
-        :raw_query   => conditions_clause,
-        :class_names => model_name,
-      }
-      opts[:filters] = @filters if @filters
-
-      if @sort_order
-        opts[:sort_mode], opts[:sort_by] = @sort_order
-      end
-
-      if limit
-        opts[:per_page] = limit
-        opts[:page]     = page
-      else
-        opts[:per_page] = MAX_SPHINX_RESULTS
-      end
-
-      if opts[:filters] and opts[:filters][id_field] and opts[:filters][id_field].empty?
+      if @filters and @filters[id_field] and @filters[id_field].empty?
         @count = 0
         @size  = 0
         @ids   = []
       else
-        before_query(opts)
-        search = Ultrasphinx::Search.new(opts)
+        search = Sphinx::Client.new
         
+        # Basic options
+        search.SetServer(server_host, server_port)
+
+        search.SetMatchMode(Sphinx::Client::SPH_MATCH_EXTENDED2)
+        if limit
+          search.SetLimits(offset, limit, offset + limit)
+        else
+          search.SetLimits(0, MAX_SPHINX_RESULTS, MAX_SPHINX_RESULTS)
+        end
+
+        search.SetSortMode(*@sort_order) if @sort_order
+
+        search.SetFilter('class_id', sphinx_class_id)
+
+        @filters and @filters.each do |field, value|
+          exclude = defined?(AntiObject) && value.kind_of?(AntiObject)
+          value = ~value if exclude
+
+          if value.kind_of?(Range)
+            min, max = filter_values([value.begin, value.end])
+            search.SetFilterRange(field.to_s, min, max, exclude)
+          else
+            search.SetFilter(field.to_s, filter_values(value), exclude)
+          end
+        end
+
+        opts = {
+          :query   => conditions_clause,
+          :filters => @filters,
+        }
+        before_query(opts)
+
         begin
-          search.run(false) # only fetch ids
+          response = search.Query(opts[:query])
         rescue Exception => e
           on_exception(e, opts)
         end
         
-        @count = search.total_entries
-        @size  = search.size
-        @ids   = search.results.collect {|model_name, id| id.to_i}.to_ordered_set
+        @count = response['total_found']
+        @ids   = response['matches'].collect {|r| r['id']}.to_ordered_set
+        @size  = @ids.size
         
         after_query(opts)
       end
     end
     
+    def filter_values(values)
+      Array(values).each do |value|
+        value.kind_of?(Date) ? value.to_time.to_i : value.to_i
+      end
+    end
+
+    def sphinx_class_id
+      Ultrasphinx::Search::MODELS_TO_IDS[model_class.to_s] || 
+      Ultrasphinx::Search::MODELS_TO_IDS[model_class.base_class.to_s]
+    end
+
+    def server_host
+      Ultrasphinx::CLIENT_SETTINGS['server_host']
+    end
+    
+    def server_port
+      Ultrasphinx::CLIENT_SETTINGS['server_port']
+    end
+
     def conditions_clause
       @conditions ? @conditions.join(' ') : ''
     end
